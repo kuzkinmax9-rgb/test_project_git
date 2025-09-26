d2fe153255b5ef801805b03c1c06ab7cf49bd309
kuzkinmax9-rgb <kuzkinmax9@gmail.com>
2025-09-26 11:25:42 +0300

    Создали класс Library и метод addBook

---
+++ my_library.js
@@ -0,0 +1,42 @@
+class Library {
+    constructor(libTitle) {
+        this.name = libTitle;
+        this.books = [];
+    }
+
+
+ //Добавляем новую книгу если 'isbn' уже есть в базе то увеличим кол-во книг 
+ addBook(title, author, year, isbn, quantity = 1) {
+        const newBook = {
+            title: title,
+            author: author,
+            year: year,
+            isbn: isbn,
+            totalQuantity: quantity,
+            availbleQuantity: quantity,
+            borrowedBy: []
+        }
+        const findBook = this.books.filter(book => book.isbn == isbn)
+        if (findBook.length) {
+            for (var i = 0; i <= this.books.length; i++) {
+                if (this.books[i] == findBook[0]) {
+                    this.books[i].totalQuantity += quantity;
+                    this.books[i].availbleQuantity += quantity;
+                }
+            }
+            // this.books.map(book => {if (book == findBook[0])
+            // {return book.totalQuantity += quantity} else
+            // {book.totalQuantity}
+            // })
+        } else{
+            this.books.push(newBook)
+        }
+        return newBook;
+    }
+}
+
+const library = new Library("Тестовая библиотека");
+library.addBook("JavaScript для начинающих", "Иван Петров", 2023, "JS-001", 5);
+library.addBook("React продвинутый", "Мария Сидорова", 2024, "REACT-002", 3);
+library.addBook("React продвинутый", "Мария Сидорова", 2024, "REACT-002", 1);
+console.log(library.books); 
